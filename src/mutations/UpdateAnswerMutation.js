bc5ea501993f8a1978baba4027291c9ccec1ec1f
root <romeubertho@gmail.com>
2020-03-16 18:29:35 -0300

    add update answer mutation

---
+++ src/mutations/UpdateAnswerMutation.js
@@ -0,0 +1,40 @@
+import {commitMutation} from 'react-relay';
+import graphql from 'babel-plugin-relay/macro';
+import environment from "../relay/Enviroment";
+
+const mutation = graphql`
+    mutation UpdateAnswerMutation($_id:ID!,$input: AnswerInput){
+        updateAnswer(_id: $_id,input: $input){
+            _id
+            answer
+        }
+    }
+`;
+
+function updateAnswer(_id, answer) {
+    const variables = {
+        _id,
+        input: {
+            answer
+        }
+    };
+    return new Promise((resolve, reject) => {
+        commitMutation(
+            environment,
+            {
+                mutation,
+                variables,
+                onCompleted: (response, errors) => {
+                    console.log(response);
+                    resolve(response)
+                },
+                onError: err => {
+                    console.log(err);
+                    reject(err)
+                }
+            }
+        );
+    });
+}
+
+export default {updateAnswer};
