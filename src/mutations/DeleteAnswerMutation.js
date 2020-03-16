b412377f3e1caa14509ddd55798285d105128543
root <romeubertho@gmail.com>
2020-03-16 17:56:19 -0300

    add deleteAnswerMutation

---
+++ src/mutations/DeleteAnswerMutation.js
@@ -0,0 +1,38 @@
+import {commitMutation} from 'react-relay';
+import graphql from 'babel-plugin-relay/macro';
+import environment from "../relay/Enviroment";
+
+
+const mutation = graphql`
+    mutation DeleteAnswerMutation($_id: ID!,$_qid:ID!){
+        deleteAnswer(_id:$_id,_qid:$_qid){
+            _id
+        }
+    }
+`;
+
+function deleteAnswer(_id, _qid) {
+    const variables = {
+        _id,
+        _qid
+    }
+    return new Promise((resolve, reject) => {
+        commitMutation(
+            environment,
+            {
+                mutation,
+                variables,
+                onCompleted: (response, errors) => {
+                    console.log(response);
+                    resolve(response);
+                },
+                onError: err => {
+                    console.log(err);
+                    reject(err)
+                }
+            }
+        );
+    })
+}
+
+export default {deleteAnswer};
