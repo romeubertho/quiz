cd253468680f230214dd534a92e15362fdf1ea1c
root <romeubertho@gmail.com>
2020-03-16 03:23:00 -0300

    rm trash code

--- src/index.js
+++ src/index.js
@@ -4,38 +4,6 @@ import './index.css';
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
 
-// import environment from "./relay/Enviroment";
-// import {graphql, QueryRenderer} from 'react-relay';
-//
-//
-// class Questions extends React.Component {
-//     render() {
-//         return (
-//             <QueryRenderer
-//                 environment={environment}
-//                 query={graphql`
-//                     query indexQuery {
-//                         allQuestions{
-//                             title
-//                         }
-//                     }
-//         `}
-//                 variables={{}}
-//                 render={({error, props}) => {
-//                     if (error) {
-//                         return <div>Error!</div>;
-//                     }
-//                     if (!props) {
-//                         return <div>Loading...</div>;
-//                     }
-//                     return <div>User ID: {props.title}</div>;
-//                 }}
-//             />
-//         );
-//     }
-// }
-
-
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
