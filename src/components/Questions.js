cfff3660fc0a6cac992ce36462867a2f06942dbf
root <romeubertho@gmail.com>
2020-03-17 00:15:22 -0300

    add delete question feature

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,14 +4,15 @@ import environment from "../relay/Enviroment";
 import {QueryRenderer} from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
 import {Link} from "react-router-dom";
+import {deleteQuestion} from '../mutations/DeleteQuestionMutation'
 
 
 class Questions extends React.Component {
     render() {
         return (
-        <QueryRenderer
-            environment={environment}
-            query={graphql`
+            <QueryRenderer
+                environment={environment}
+                query={graphql`
                     query QuestionsQuery {
                         allQuestions{
                             _id
@@ -23,58 +24,67 @@ class Questions extends React.Component {
                         }
                     }
                 `}
-            variables={{}}
-            render={({error, props}) => {
-                if (error) {
-                    return <div>{error.message}</div>;
-                }
-                if (!props) {
-                    return <div>Loading...</div>;
-                }
-                return (
-                    <div className="container m-t-20">
-                        <h1 className="page-title">Questions</h1>
-                        <div className="allquestions-page">
-                            <div className="columns is-multiline">
-                                {props.allQuestions.length > 0
-                                    ? props.allQuestions.map(questions => (
-                                        <div className="column is-one-third" key={questions._id}>
-                                            <div className="card">
-                                                <header className="card-header">
-                                                    <p className="card-header-title">{questions.title}</p>
-                                                </header>
-                                                <div className="card-content">
-                                                    <div className="content">
-                                                        {/*{console.log(questions)}*/}
-                                                        {questions.answers.length>0?questions.answers.map(answer =>(
+                variables={{}}
+                render={({error, props}) => {
+                    if (error) {
+                        return <div>{error.message}</div>;
+                    }
+                    if (!props) {
+                        return <div>Loading...</div>;
+                    }
+                    return (
+                        <div className="container m-t-20">
+                            <h1 className="page-title">Questions</h1>
+                            <div className="allquestions-page">
+                                <div className="columns is-multiline">
+                                    {props.allQuestions.length > 0
+                                        ? props.allQuestions.map(questions => (
+                                            <div className="column is-one-third" key={questions._id}>
+                                                <div className="card">
+                                                    <header className="card-header">
+                                                        <p className="card-header-title">{questions.title}</p>
+                                                    </header>
+                                                    <div className="card-content">
+                                                        <div className="content">
+                                                            {/*{console.log(questions)}*/}
+                                                            {questions.answers.length > 0 ? questions.answers.map(answer => (
                                                                 <li key={answer._id}>
                                                                     {answer.answer}
                                                                 </li>
-                                                        )):'No answers created yet'
-                                                        }
-                                                        <br/>
+                                                            )) : 'No answers created yet'
+                                                            }
+                                                            <br/>
+                                                        </div>
                                                     </div>
+                                                    <footer className="card-footer">
+                                                        {/*<Link to={`edit/${questions._id}`} className="card-footer-item">*/}
+                                                        {/*    Edit*/}
+                                                        {/*</Link>*/}
+                                                        <button className="card-footer-item"
+                                                                onClick={() => console.log(questions._id)}>
+                                                            Edit
+                                                        </button>
+                                                        <button className="card-footer-item"
+                                                                onClick={() => deleteQuestion(questions._id)}>
+                                                            Delete
+                                                        </button>
+                                                        {/*<Link to={`delete/${questions._id}`}*/}
+                                                        {/*      className="card-footer-item">*/}
+                                                        {/*    Delete*/}
+                                                        {/*</Link>*/}
+                                                    </footer>
                                                 </div>
-                                                <footer className="card-footer">
-                                                    <Link to={`edit/${questions._id}`} className="card-footer-item">
-                                                        Edit
-                                                    </Link>
-                                                    <Link to={`delete/${questions._id}`} className="card-footer-item">
-                                                        Delete
-                                                    </Link>
-                                                </footer>
                                             </div>
-                                        </div>
-                                    ))
-                                    : "No Questions yet"}
+                                        ))
+                                        : "No Questions yet"}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                );
-            }}
-    />
-    )
-        ;
+                    );
+                }}
+            />
+        )
+            ;
     }
 }
 
