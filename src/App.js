5171962ae0c812bf9ac39ff5abbcef9e2cc6076a
root <romeubertho@gmail.com>
2020-03-20 00:40:15 -0300

    Add new question modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './App.css';
 import Questions from "./components/Questions";
 import {Breadcrumb, Layout, Menu} from "antd";
+import Input from "./components/Input";
 
 const {Header, Content, Footer} = Layout;
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalVisible: React.createRef()
+        }
+    }
+    setModalVisible(modalVisible) {
+        this.state.modalVisible.current.setModalVisible(modalVisible)
+    }
     render() {
         return (
             <Layout>
@@ -16,7 +26,7 @@ class App extends React.Component {
                         mode="horizontal"
                         defaultSelectedKeys={['2']}
                         style={{lineHeight: '64px'}}>
-                        <Menu.Item key="1">New Question</Menu.Item>
+                        <Menu.Item onClick={() =>this.setModalVisible(true)} key="1">New Question</Menu.Item>
                     </Menu>
                 </Header>
                 <Content className="site-layout" style={{padding: '0 50px', marginTop: 64}}>
@@ -24,6 +34,7 @@ class App extends React.Component {
                     </Breadcrumb>
                     <div className="site-layout-background" style={{padding: 0, minHeight: 380}}>
                         <Questions/>
+                        <Input ref={this.state.modalVisible}/>
                     </div>
                 </Content>
                 <Footer style={{textAlign: 'center'}}>Question Manager</Footer>
